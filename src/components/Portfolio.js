9913d3c580ad8fafa66e696a4b79c0029eb74306
Shannon Rivers <shannon.rivers@gmail.com>
2017-10-05 11:59:48 -0600

    refine Modal display, add CSS orbit to list

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import '../App.css';
-import Viewer from './Viewer';
+import Viewer from './Viewer.js';
 import { Button } from 'reactstrap';
+import Modal from 'react-modal';
+
 
 export default class Portfolio extends Component {
   constructor(props){
@@ -31,7 +33,7 @@ export default class Portfolio extends Component {
 
   toggleModal=()=>{
     this.setState({
-      hideModal: false
+      hideModal: !this.state.hideModal
     });
   };
 
@@ -39,27 +41,77 @@ export default class Portfolio extends Component {
     this.fetchPlaces();
   }
 
-  siteListItem=(website)=>{
+  siteListItem=(website, index)=>{
     let passVal = website.url;
-    return (
-      <li
-      onClick={(e)=>this.handleSiteClick(e, passVal)}
-      key={website.id}
-      value={website.url}>
-      {website.title}
-      </li>
-    )
+    switch (index){
+      case 0:
+      case 1:
+      case 2:
+        return (
+          <div id="inner-orbit" key={website.id}>
+            <div className="inner-orbit-cirlces"
+            onClick={(e)=>this.handleSiteClick(e, passVal)}
+            key={website.id}
+            value={website.url}>
+              {website.title}
+            </div>
+          </div>
+        )
+        // break;
+      case 3:
+      case 4:
+      case 5:
+        return (
+          <div id="middle-orbit" key={website.id}>
+            <div className="middle-orbit-cirlces"
+            onClick={(e)=>this.handleSiteClick(e, passVal)}
+            key={website.id}
+            value={website.url}>
+              {website.title}
+            </div>
+          </div>
+        )
+        // break;
+      case 6:
+      case 7:
+      case 8:
+        return (
+          <div id="outer-orbit" key={website.id}>
+            <div className="outer-orbit-cirlces"
+            onClick={(e)=>this.handleSiteClick(e, passVal)}
+            key={website.id}
+            value={website.url}>
+              {website.title}
+            </div>
+          </div>
+        )
+        // break;
+      default:
+        return (
+          <div id="outer-orbit" key={website.id}>
+            <div className="outer-orbit-cirlces"
+            onClick={(e)=>this.handleSiteClick(e, passVal)}
+            key={website.id}
+            value={website.url}>
+              {website.title}
+            </div>
+          </div>
+        )
+        // break;
+    }
   }
 
   render() {
     return (
       <div className="portfolio">
         <h3>this is some of the neat stuff <a href="https://github.com/senbenito">senbenito</a> has crafted:</h3>
-        <ul>
-        {this.state.websites.map((website) => this.siteListItem(website))}
-        </ul>
-        <Button onClick={this.toggleModal}>Click</Button>
-        <Viewer url={this.state.viewerURL}/>
+        <div id="circle-orbit-container">
+          {this.state.websites.map((website, index) => this.siteListItem(website, index))}
+        </div>
+        <Modal isOpen={!this.state.hideModal} contentLabel="Modal">
+          <Viewer viewerURL={this.state.viewerURL}/>
+          <Button onClick={this.toggleModal}/>
+        </Modal>
       </div>
     );
   }
--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import Modal from 'react-modal';
-
 
 export default class Viewer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      url: this.props.url,
-      hideModal: true
+      viewerURL: this.props.viewerURL,
     };
   };
 
   render(){
     return(
-       <div>
-         <Modal isOpen={!this.state.hideModal} contentLabel="Modal">
-             <iframe
-              id="player"
-              title="jumbotron"
-              type="text/html"
-              width="1200"
-              height="400"
-              src= {this.state.url}
-              frameBorder="10">
-             </iframe>
-         </Modal>
-       </div>
+      <div>
+        <iframe
+          id="player"
+          title={this.state.viewerURL}
+          type="text/html"
+          width="800"
+          height="500"
+          src= {this.state.viewerURL}
+          frameBorder="10">
+        </iframe>
+      </div>
     )
   };
 };
