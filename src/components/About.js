fc4d537ffae7db37727c0e73b64b9e876ddafd01
Shannon Rivers <shannon.rivers@gmail.com>
2017-10-19 13:21:21 -0600

    replace showdown with marked + JS file - displays README as HTML; needs styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-// import { Converter } from 'showdown';
 import 'react-toastify/dist/ReactToastify.min.css';
 import '../App.css';
-
-const showdown  = require('showdown'),
-    converter = new showdown.Converter(),
-    text      = `# About senbenito's portfolio
-           This repoistory started life as a 15-minute warmup during my [Web Development Immersive at Galvanize, Boulder](https://www.galvanize.com/boulder/web-development "Galvanize Boulder shoutout").
-           ##### Learn more about my time @ Galvanize: [const senbenito = { ... g[49].16 }](https://talent.galvanize.com/students/1556 "Galvanize Talent link")
-
-           Originally intended as a simple listing of select, deployed projects, this portfolio will be used to document my on-going developer development as I learn and apply new technologies.`,
-    html      = converter.makeHtml(text);
 
 export default class About extends React.Component{
   constructor(props){
      super(props);
      this.state = {
-       readme: html,
      };
    };
 
-  handleChange=(event)=>{
-    const value = event.target.value;
-    const name = event.target.name;
-    this.setState({
-      [name]: value
-    });
-  };
+  // handleChange=(event)=>{
+  //   const value = event.target.value;
+  //   const name = event.target.name;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
   notify=(message)=>toast(message);
 
@@ -43,7 +32,7 @@ export default class About extends React.Component{
          closeOnClick
          pauseOnHover
         />
-        {this.state.readme}
+        <object data="README.html" type="text/html"><a href="https://github.com/senbenito/portfolio-frontend/blob/master/README.md">GitHub</a></object>
       </div>
     )
    }
