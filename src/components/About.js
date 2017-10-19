765ae27936602c0557f03e2d3986dc218f29aa67
Shannon Rivers <shannon.rivers@gmail.com>
2017-10-19 00:18:56 -0600

    add markdown to html converter, needs massaging to display

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+// import { Converter } from 'showdown';
 import 'react-toastify/dist/ReactToastify.min.css';
 import '../App.css';
+
+const showdown  = require('showdown'),
+    converter = new showdown.Converter(),
+    text      = `# About senbenito's portfolio
+           This repoistory started life as a 15-minute warmup during my [Web Development Immersive at Galvanize, Boulder](https://www.galvanize.com/boulder/web-development "Galvanize Boulder shoutout").
+           ##### Learn more about my time @ Galvanize: [const senbenito = { ... g[49].16 }](https://talent.galvanize.com/students/1556 "Galvanize Talent link")
+
+           Originally intended as a simple listing of select, deployed projects, this portfolio will be used to document my on-going developer development as I learn and apply new technologies.`,
+    html      = converter.makeHtml(text);
 
 export default class About extends React.Component{
   constructor(props){
      super(props);
      this.state = {
-       url: '',
-       title: ''
+       readme: html,
      };
    };
 
@@ -34,15 +43,7 @@ export default class About extends React.Component{
          closeOnClick
          pauseOnHover
         />
-        <iframe
-          id="player"
-          title="about"
-          type="text/html"
-          width="1200"
-          height="400"
-          src="../README.md"
-          frameBorder="10">
-        </iframe>
+        {this.state.readme}
       </div>
     )
    }
